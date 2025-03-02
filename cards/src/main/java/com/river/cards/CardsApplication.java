2df93a702ea7321e9c4d623cef74356c301d8c20
ttgiang275 <ttgiang275@gmail.com>
2025-03-02 15:50:55 +0700

    add cards and loans services

---
+++ cards/src/main/java/com/river/cards/CardsApplication.java
@@ -0,0 +1,13 @@
+package com.river.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardsApplication.class, args);
+    }
+
+}
