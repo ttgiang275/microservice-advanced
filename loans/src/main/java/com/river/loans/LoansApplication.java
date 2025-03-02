f46a8b620c5c0498c0ac40c9006ab3161f909fc2
ttgiang275 <ttgiang275@gmail.com>
2025-03-02 16:13:35 +0700

    implement loans service

--- loans/src/main/java/com/river/loans/LoansApplication.java
+++ loans/src/main/java/com/river/loans/LoansApplication.java
@@ -1,9 +1,36 @@
 package com.river.loans;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "River",
+                        email = "river@gmail.com",
+                        url = "https://www.river.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.river.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Loans microservice REST API Documentation",
+                url = "https://www.river.com/swagger-ui.html"
+        )
+)
 public class LoansApplication {
 
     public static void main(String[] args) {
